ceaace161bd95fb61041b15577458ad25a8ca2e6
Raymond Nirnberger <rnirnber@gmail.com>
2026-08-17 18:51:00 -0700

    a

--- CalciumSDK/DoMainAction.cs
+++ CalciumSDK/DoMainAction.cs
@@ -33,19 +33,27 @@ namespace CalciumSDK
                         Console.Out.Flush();
                     });
                     Thread.Sleep(1000 * 2);
-
-                    var all_assets = new StringBuilder();
+                    
                     pending_changes.ForEach((pc) =>
                     {
                         var path = Helpers.GET_ROOT_SDK_PATH() + Path.DirectorySeparatorChar + SELECTED_PROJECT + Path.DirectorySeparatorChar + "assets" + Path.DirectorySeparatorChar + pc + ".bmp";
                         var bytes = File.ReadAllBytes(path);
                         var hash = Helpers.GetDigest(bytes);
                         File.WriteAllText(path.Replace(".bmp", ".signature"), hash);
-
-                        var asset_sb = Assets.GenerateAssetPPL(path);
-                        all_assets.Append(asset_sb);
                     });
-                    File.WriteAllText(Helpers.GET_ROOT_SDK_PATH() + Path.DirectorySeparatorChar + SELECTED_PROJECT + Path.DirectorySeparatorChar + "assets.ppl", all_assets.ToString());
+                    var all_assets = new StringBuilder();
+                    for (int i = 0; i < 9999; i++)
+                    {
+                        var bmp_path = Helpers.GET_ROOT_SDK_PATH() + Path.DirectorySeparatorChar + SELECTED_PROJECT +
+                                       Path.DirectorySeparatorChar + "assets" + Path.DirectorySeparatorChar +
+                                       "asset_" + Helpers.GetPaddedNum(i) + ".bmp";
+                        
+                        if (File.Exists(bmp_path))
+                        {
+                            all_assets.Append(Assets.GenerateAssetPPL(bmp_path));
+                        }
+                    }
+                    File.WriteAllText(Helpers.GET_ROOT_SDK_PATH() + Path.DirectorySeparatorChar + SELECTED_PROJECT + Path.DirectorySeparatorChar + "assets.ppl_DO_NOT_EDIT", all_assets.ToString());
                     break;
                 case "B":
                     var success2 = Preflight.VerifyScenesForHydration(projectName);
